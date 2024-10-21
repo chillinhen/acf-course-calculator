d8e56e4868eccb97a401ffebc988661676418431
chillinhen <contact@chilliscope.de>
2024-10-21 10:10:08 +0200

    initial setup

---
+++ acf-course-calculator.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Plugin Name: ACF Course Calculator
+Description: Individuelles Plugin für spraachen.org zur Berechnung der Kurskosten und Rabatte, einzubinden auf Kurseiten
+Version: 1.0
+Author: Claudia Hillen
+*/
